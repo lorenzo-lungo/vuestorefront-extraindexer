6b2e762c2e3d42f901809f64269e1f85e8aee5ec
Lorenzo.lungo <lorenzo.lungo@triboo.it>
2019-12-16 10:08:09 +0100

    Major logic improvement and related README

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ export const registerModules: VueStorefrontModule[] = [
 ]
 ``` 
 
-#### For the widgetwall you can add that to your theme components, for categories you only need to import it as always while for cms pages you have 2 ways
+#### For the widgetwall.vue you can add that to your theme components, for categories you only need to import it with `<WidgetWall :parent_id="categoryId" />` while for cms pages you have 2 ways
 
 ##### 1) in vue-storefront/src/themes/default/router/index.js or the router of your theme pass in the prop the group cms like this `props: {page: 'faq', title: 'faq', group: 'cms'}`
 ##### 2) in the component where you are importing the WidgetWall add 
@@ -22,3 +22,4 @@ props: {
     }
   }
 ``` 
+#### Either way be sure your component has a page prop and a group prop so you can add the component with `<WidgetWall :group="$props.group" :page="$props.page" />`
--- WidgetWall.vue
+++ WidgetWall.vue
@@ -1,60 +1,83 @@
 <template>
   <div id="widgetwall">
-    <div v-if="parent === 'cms' ">
-      <li v-for="widget in lists">
+    <div v-if="$props.group ">
+      <li v-for="widget in lists" :key="widget.id">
         {{ widget.title }}
       </li>
     </div>
-    <div v-if="category">
-      <li v-for="widget in lists">
+    <div v-if="$props.parent_id">
+      <li v-for="widget in lists" :key="widget.id">
         {{ widget.title }}
       </li>
     </div>
   </div>
 </template>
 <script>
-import { extraQuickSearchByQuery } from 'src/modules/extra-indexer/components/search'
-import SearchQuery from '@vue-storefront/core/lib/search/searchQuery'
-import { Logger } from '@vue-storefront/core/lib/logger'
-import _ from 'lodash'
+  import { extraQuickSearchByQuery } from 'src/modules/extra-indexer/components/search'
+  import SearchQuery from '@vue-storefront/core/lib/search/searchQuery'
+  import { Logger } from '@vue-storefront/core/lib/logger'
+  import _ from 'lodash'
 
-export default {
-  name: 'WidgetWall',
-  data () {
-    return {
-      lists: [],
-      grouped: [],
-      parent: '',
-      category: ''
-    }
-  },
-  created () {
-    console.log(this.$parent._props)
-    if (this.$parent._props && this.$parent._props.group === 'cms') {
-      this.parent = this.$parent._props.group
-      let query = new SearchQuery()
-      let path = this.$root._route.fullPath.split('/').pop()
-      query.applyFilter({ key: 'route_parameters.route', value: { 'eq': path }})
-      return extraQuickSearchByQuery({ query, entityType: 'widget_instance' })
-        .then((resp) => {
-          this.lists = resp.items
-        })
-        .catch(err => {
-          Logger.error(err, 'faq')()
-        })
-    }
-    if (this.$parent.categoryId) {
-      this.category = this.$parent.categoryId
-      let query = new SearchQuery()
-      query.applyFilter({ key: 'route_parameters.category', value: { 'in': this.$parent.categoryId }})
-      return extraQuickSearchByQuery({ query, entityType: 'widget_instance' })
-        .then((resp) => {
-          this.lists = resp.items
-        })
-        .catch(err => {
-          Logger.error(err, 'faq')()
-        })
+  export default {
+    name: 'WidgetWall',
+    props: {
+      parent_id: {
+        type: Number
+      },
+      group: {
+        type: String
+      },
+      page: {
+        type: String
+      }
+    },
+    data () {
+      return {
+        lists: []
+      }
+    },
+    beforeMount () {
+      this.getList()
+      this.getCmsList()
+    },
+    watch: {
+      parent_id () {
+        this.getList()
+      },
+      page () {
+        this.getCmsList()
+      }
+    },
+    methods: {
+      getList () {
+        if (this.$props.parent_id) {
+          let query = new SearchQuery()
+          query.applyFilter({key: 'cat_parameters', value: {'in': this.$props.parent_id}})
+          return extraQuickSearchByQuery({query, entityType: 'widget_instance'})
+                  .then((resp) => {
+                    console.log(resp.items)
+                    this.lists = resp.items
+                  })
+                  .catch(err => {
+                    Logger.error(err, 'widget')()
+                  })
+        }
+      },
+      getCmsList () {
+        if (this.$props.group && this.$props.group === 'cms') {
+          let query = new SearchQuery()
+          console.log(this.$root._route.fullPath.split('/').pop())
+          let path = this.$root._route.fullPath.split('/').pop()
+          query.applyFilter({ key: 'decoded_parameters.route', value: { 'eq': path }})
+          return extraQuickSearchByQuery({ query, entityType: 'widget_instance' })
+                  .then((resp) => {
+                    this.lists = resp.items
+                  })
+                  .catch(err => {
+                    Logger.error(err, 'faq')()
+                  })
+        }
+      }
     }
   }
-}
 </script>
